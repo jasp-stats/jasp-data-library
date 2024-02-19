3aa1bc34118d962f6301c0dd30c6403ae0c095c9
RDOFFERHOFF <rdofferhoff@protonmail.com>
2024-02-19 13:42:22 +0100

    add little script to update the yml

---
+++ .scripts/gen_index.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+import yaml
+    
+
+def main():
+    chapterdir = Path('./myChapters/')
+    chapters = [str(x) for x in chapterdir.glob("*.md")]
+    with open("_quarto.yml", 'r+') as f:
+        current = yaml.safe_load(f)
+        current['book']['chapters'] = ['index.qmd'] + list(chapters)
+        f.seek(0)
+        yaml.dump(current, f)
+
+if __name__ == "__main__":
+    main()
